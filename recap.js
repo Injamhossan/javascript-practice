c4309325ca97023d1d64107cfe6a210373a8f466
Injamhossan <injamhossanmamun@gmail.com>
2025-08-06 20:41:58 +0600

    some task add

---
+++ recap.js
@@ -0,0 +1,23 @@
+const books = ['Physics', 'Chemistry', 'Biology', 'Mathematics', 'English'];
+
+for(let i = 0; i < books.length; i++) {
+    console.log(books[i]);
+}
+
+const rev_books = books.reverse();
+
+const sorted_books = books.sort();
+
+const gadget = [
+    { name: 'laptop', price: 100000, color: 'black'},
+    { name: 'phone', price: 50000, color: 'blue'},
+    { name: 'watch', price: 20000, color: 'silver'}
+]
+
+// 2D array 
+
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
