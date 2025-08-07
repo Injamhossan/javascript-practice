ca391838343b4709a707ca2c81c4d8cb101fa699
Injamhossan <injamhossanmamun@gmail.com>
2025-08-08 01:07:18 +0600

    Argument with function

---
+++ arguments.js
@@ -0,0 +1,9 @@
+function diffrence (num1, num2) {
+    const diff = num1 - num2;
+    console.log(num1, num2, 'Difference is', diff);
+}
+
+const fatherAge = 40;
+const motherAge = 38;
+
+diffrence(fatherAge, motherAge);
